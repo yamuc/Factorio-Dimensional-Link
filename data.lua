0f5ea4fe2948fed6734a7d2139eb35ea6914befa
yamuc <gavinlesher5501@gmail.com>
2024-07-10 18:30:09 -0700

    Got basic functionality for items, recipies and entities in

--- data.lua
+++ data.lua
@@ -20,8 +20,123 @@ local function sound(name)
   end
 
 
+-- items
+data:extend({
+  {
+    --item info
+    type = 'item',
+    name = 'yc:test-item',
 
+    --sprite stuff
+    icon = sprite 'advanced-fuel.png',
+    icon_size = 64,
 
+    --inventory stuff
+    subgroup = 'energy', 
+    order = 'y-a',
+
+    --other
+    place_result = 'yc:test-item',
+    stack_size = 10
+  }
+})
+
+-- recipies
+data:extend({
+  {
+    type = 'recipe',
+    name = 'yc:test-item',
+
+    enabled = true,
+    ingredients = {
+      {'iron-plate', 50},
+      {'iron-ore', 10}
+    },
+
+    results = {
+      {type = 'item', name = 'yc:test-item', amount = 1},
+      {type = 'item', name = 'iron-plate', probability = .5, amount = 10}
+    },
+    main_product = 'yc:test-item',
+
+    energy_required = .5
+  }
+})
+
+-- entities
+local kr_entities_path = sprite ''
+data:extend({
+  {
+    type = 'assembling-machine',
+    name = 'yc:test-item',
+    icon = sprite 'advanced-chemical-plant.png',
+    icon_size = 64,
+    icon_mipmaps = 4,
+    flags = { "placeable-neutral", "placeable-player", "player-creation" },
+    minable = { mining_time = 1, result = "yc:test-item" },
+    max_health = 1500,
+    collision_box = { { -3.25, -3.25 }, { 3.25, 3.25 } },
+    selection_box = { { -3.5, -3.5 }, { 3.5, 3.5 } },
+    animation = {
+      layers = {
+        {
+          filename = kr_entities_path .. "advanced-chemical-plant/advanced-chemical-plant.png",
+          priority = "high",
+          width = 226,
+          height = 268,
+          shift = { 0, -0.48 },
+          frame_count = 20,
+          line_length = 5,
+          animation_speed = 0.25,
+          scale = 1,
+          hr_version = {
+            filename = kr_entities_path .. "advanced-chemical-plant/hr-advanced-chemical-plant.png",
+            priority = "high",
+            width = 451,
+            height = 535,
+            shift = { 0, -0.48 },
+            frame_count = 20,
+            line_length = 5,
+            animation_speed = 0.25,
+            scale = 0.5,
+          },
+        },
+        {
+          filename = kr_entities_path .. "advanced-chemical-plant/advanced-chemical-plant-sh.png",
+          priority = "high",
+          width = 258,
+          height = 229,
+          shift = { 0.33, 0.32 },
+          frame_count = 1,
+          repeat_count = 20,
+          animation_speed = 0.25,
+          scale = 1,
+          draw_as_shadow = true,
+          hr_version = {
+            filename = kr_entities_path .. "advanced-chemical-plant/hr-advanced-chemical-plant-sh.png",
+            priority = "high",
+            width = 516,
+            height = 458,
+            shift = { 0.33, 0.32 },
+            frame_count = 1,
+            repeat_count = 20,
+            animation_speed = 0.25,
+            scale = 0.5,
+            draw_as_shadow = true,
+          },
+        },
+      },
+    },
+    crafting_speed = 1,
+    energy_source = {
+      type = "electric",
+      usage_priority = 'secondary-input'
+    },
+    energy_usage = "1.75MW",
+    ingredient_count = 6,
+    crafting_categories = {'crafting', 'chemistry', 'basic-crafting'}
+  }
+})
 
 
 
